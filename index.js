448b05db9718f5e62d76f19c7804b7ba1b5481f8
KienTran17 <kien.tdt.95@gmail.com>
2017-03-10 11:04:32 +0700

    first index

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 let express = require('express');
 let app = express();
-app.listen('3000', ()=> console.log('server started'));
+app.listen('1000', ()=> console.log('server started'));
 
 let parser = require('body-parser').urlencoded({extended:false});
 
 app.set('view engine','ejs');
 app.set('views','./views');
+
+app.use(express.static('public'));
+
+app.get('/', parser, (req,res)=>{
+    res.render('./front-end/index');
+});

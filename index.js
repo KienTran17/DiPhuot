6f143543bd35f3d7679698703956d47c125293c2
DESKTOP-3N3M1M7\KienTran <kien.tdt.95@gmail.com>
2017-05-10 15:58:01 +0700

    make it better

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(session({
     }
 }));
 app.use(cookieParser());
-app.listen('3000', () => console.log('server started'));
+app.listen(process.env.PORT || '3000');
 
 
 //gói body-parser lấy dữ liệu từ form (POST) trong ejs
